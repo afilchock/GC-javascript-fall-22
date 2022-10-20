67afe1405aaa60a6cc6abe565e99304bf4e13a18
afilchock <afilchock@gmail.com>
2022-10-20 17:33:51 -0400

    image uploads

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // alert('My project is going to be awesome!');
+$("#noShow").hide();
+$("h1").html("30 Rock Songs!");
+// modify css styles for the button
+$("#toggleSongs").css({
+    "background-color": "orange",
+    "color": "white",
+    "font-size": "20px",
+    "padding": "10px",
+    "border-radius": "5px",
+    "border": "2px solid black",
+    "margin": "10px"
+});
+$("#toggleSongs").click(function() {
+    $("#song").toggle();
+  });
